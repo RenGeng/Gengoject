389983c6f49545c2af3469c9a94ec6d3116f9af6
lance <lancesatge@hotmail.fr>
2016-12-07 22:15:37 +0100

    pour toi gengo

--- template.c
+++ template.c
@@ -20,48 +20,31 @@ typedef struct{
    int x;
    int y;
    int energie;
-} joueur;
-
+} t_joueur;
 extern int debug;	/* hack to enable debug messages */
 
-void affichage_manuel(char **tabData,joueur p1,joueur p2,int sizeX,int sizeY)
+/*void affichage_manuel(char *labData,int pos_J0,int pos_J1,int pos_tresor,int sizeX,int sizeY)
 {
-   /* int i; */
-   /* labData[pos_J0]='>'; */
-   /* labData[pos_J1]='@'; */
-   /* for(i=0;i<(sizeX*sizeY);i++)  */
-   /* { */
-   /*    if(i%sizeX==0) printf("\n"); */
-   /*    if (i==pos_J0 || i==pos_J1 || i==pos_tresor)  */
-   /*    { */
-   /* 	 putchar(labData[i]); */
-   /* 	 printf("|"); */
-   /*    } */
-   /*    else printf("%d|",labData[i]); */
-   /* } */
-   /* labData[pos_J0]=0; */
-   /* labData[pos_J1]=0;       */
-   /* printf("\n"); */
-   
-   /* Problème d'affichage mais le tableau est bien initialisé */
-   tabData[p1.x][p1.y] = '>';
-   tabData[p2.x][p2.y] = '@';
-   int i,j;
-   for(i=0;i<sizeY;i++)
+   int i;
+   labData[pos_J0]='>';
+   labData[pos_J1]='@';
+   for(i=0;i<(sizeX*sizeY);i++) 
    {
-      for(j=0;j<sizeX;j++) 
-	 {
-	    if (j%sizeX==0) printf("\n");
-	    if ((i==p1.y && j==p1.x) || (i==p2.y && j==p2.x)) putchar(tabData[i][j]);
-	    else printf("%d|", tabData[i][j]);
-	 }
+      if(i%sizeX==0) printf("\n");
+      if (i==pos_J0 || i==pos_J1 || i==pos_tresor) 
+      {
+	 putchar(labData[i]);
+	 printf("|");
+      }
+      else printf("%d|",labData[i]);
    }
+   labData[pos_J0]=0;
+   labData[pos_J1]=0;      
    printf("\n");
-   tabData[p1.x][p1.y] = 0;
-   tabData[p2.x][p2.y] = 0;
-}
 
-char** init_tab(char *labData,int sizeX, int sizeY)
+   }*/
+
+char** init_lab(char *labData,t_joueur p1,t_joueur p2,int sizeX, int sizeY)
 {
    /*initialisation du tableau 2D */
    char** tabData;
@@ -70,11 +53,50 @@ char** init_tab(char *labData,int sizeX, int sizeY)
    for (i=0;i<sizeY;i++) tabData[i] = (char*) malloc(sizeX*sizeof(char));
    for (i=0;i<sizeY;i++)
    {
-      for (j=0;j<sizeX;j++) tabData[i][j] = labData[i*sizeX+j];
+      for (j=0;j<sizeX;j++) 
+      {
+	 if((i==p1.y && j==p1.x) || (i==p2.y && j==p2.x) || (i==sizeY/2 && j==sizeX/2))
+	 {
+	    if((i==p1.y && j==p1.x)) tabData[i][j]='>';
+	    else if((i==p2.y && j==p2.x)) tabData[i][j]='@';
+	    else if((i==sizeY/2 && j==sizeX/2)) tabData[i][j]='T';
+	    putchar(tabData[i][j]);
+	    printf("|");
+	 }
+	 else
+	 {	    
+	    tabData[i][j] = labData[i*sizeX+j];
+	    printf("%d|",tabData[i][j]);
+	 }
+      }
+      printf("\n");
    }
    return tabData;
-
 }
+
+void affichage_2D(char** labData_2D,t_joueur p1,t_joueur p2,int sizeX,int sizeY)
+{
+   int i,j;   
+   for(i=0;i<sizeY;i++)
+   {
+      for(j=0;j<sizeX;j++)
+      {
+	 if((i==p1.y && j==p1.x) || (i==p2.y && j==p2.x) || (i==sizeY/2 && j==sizeX/2)) 
+	 {
+	    putchar(labData_2D[j][i]);
+	    printf("|");
+	 }
+	    else 
+	    {
+	       //putchar(labData_2D[i][j]);
+	       //printf("|");
+	       printf("%d|",labData_2D[i][j]);
+      
+	    }
+      }
+      printf("\n");
+   }
+   }
 
 char *maj_lab(char *labData,t_move move,int sizeX,int sizeY)
 {
@@ -113,46 +135,42 @@ char *maj_lab(char *labData,t_move move,int sizeX,int sizeY)
    return labData;
    
 }
+
+
 int main()
 {
    srand(time(NULL));
    char labName[50];					/* name of the labyrinth */
    char* labData;						/* data of the labyrinth */
-   char** tabData;						/* Déclaration du tableau 2D */
+   char** labData_2D; //labData en 2D
    t_return_code ret = MOVE_OK;		/* indicates the status of the previous move */
    t_move move;						/* a move */
    int player;
    int sizeX,sizeY;
-   joueur p1, p2; 		/* Initialisation de structure des 2 joueurs */
+   t_joueur p1,p2;
 
    /* connection to the server */
    connectToServer( "pc4023.polytech.upmc.fr", 1234, "Gengo_Lance");
 	
 	
    /* wait for a game, and retrieve informations about it */
-   waitForLabyrinth( "DO_NOTHING timeout=6000", labName, &sizeX, &sizeY);
+   waitForLabyrinth( "PLAY_RANDOM timeout=60", labName, &sizeX, &sizeY);
    labData = (char*) malloc( sizeX * sizeY );
    player = getLabyrinth(labData);
-   tabData = init_tab(labData,sizeX,sizeY);
-
-   printf("sizeX=%d et sizeY=%d\n",sizeX,sizeY);
+   printf("sizeX=%d et sizeY=%d\n",sizeX,sizeY); 
+	
+	
+	
    printf("\n");
    //printf("L%d,R%d,U%d,D%d\n",ROTATE_LINE_LEFT,ROTATE_LINE_RIGHT,ROTATE_COLUMN_UP,ROTATE_COLUMN_DOWN);
    int mv; //mouvement choisi
    int pos_J0,pos_J1,pos_tresor; //position des joueurs en 1D
    pos_tresor=sizeX*(sizeY/2)+sizeX/2;
-   if(player==0)  		/* Si l'adversaire commence */
+   if(player==0) 
    {
-      p2.x = 0;			/* L'adversaire est à gauche */
-      p2.y = sizeY/2;
-      p2.energie = 0; 		/* Il commence avec 0 energie */
-      
-      p1.x = sizeX-1;		/* On commence à droite */
-      p1.y = sizeY/2;
-      p1.energie = 1;		/* On commence avec 1 energie */
-   }
-   else
-   {
+      pos_J0=sizeX*(sizeY/2); 
+      pos_J1=sizeX+pos_J0-1;
+
       p1.x = 0;			/* On est à gauche */
       p1.y = sizeY/2;
       p1.energie = 0; 		/* On commence avec 0 energie */
@@ -161,23 +179,42 @@ int main()
       p2.y = sizeY/2;
       p2.energie = 1;		/* L'adversaire commence avec 1 energie */
    }
+   else
+   {
+      pos_J1=sizeX*(sizeY/2); 
+      pos_J0=sizeX+pos_J1-1;
+
+      p2.x = 0;			/* L'adversaire est à gauche */
+      p2.y = sizeY/2;
+      p2.energie = 0; 		/* Il commence avec 0 energie */
+      
+      p1.x = sizeX-1;		/* On commence à droite */
+      p1.y = sizeY/2;
+      p1.energie = 1;		/* On commence avec 1 energie */
+   }
 
    //labData[pos_tresor]='T';
-   // affichage_manuel(tabData,p1,p2,sizeX,sizeY);
+   labData_2D=init_lab(labData,p1,p2,sizeX,sizeY);
+   
+   //affichage_2D(labData_2D,p1,p2,sizeX,sizeY);
+   //affichage_manuel(labData,pos_J0,pos_J1,pos_tresor,sizeX,sizeY);
    //printLabyrinth();
-	
-   do
+   
+   /*do
    {
-      affichage_manuel(tabData,p1,p2,sizeX,sizeY);
       if(player==0)
       {
 	 system("clear");
 	 printLabyrinth();
+	 affichage_manuel(labData,pos_J0,pos_J1,pos_tresor,sizeX,sizeY);
+	 printf("\n");
+	 //affichage_2D(labData_2D,p1,p2,sizeX,sizeY);
+	 printf("sizeX=%d sizeY=%d\np1x:%d p1y:%d\np2x:%d p2y:%d\n",sizeX,sizeY,p1.x,p1.y,p2.x,p2.y);
       }
       /* display the labyrinth */	      
 	
-      if (player==1)	/* The opponent plays */
-      {
+   // if (player==1)	/* The opponent plays */
+	 /* {
 	 ret = getMove( &move);
 	 if(move.type<=3) labData=maj_lab(labData,move,sizeX,sizeY);
 	 player=0;
@@ -277,4 +314,3 @@ int main()
 	
    return EXIT_SUCCESS;
 }
-	
