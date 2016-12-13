96b5cee63fb18daa5f91c4d6f8cfd3fd1b7ebc47
lance <lancesatge@hotmail.fr>
2016-12-13 17:08:10 +0100

    Rotation gauche et droite bon, mais des choses à corriger

--- template.c
+++ template.c
@@ -6,6 +6,7 @@
 // réceptionnant le coup de l'adversaire s'il ne commence pas) 
 // et termine le jeu.
 // Ce programme vous sert de base pour construire votre propre programme
+//< pos[p1.y][p1.x]
 
 
 
@@ -15,16 +16,20 @@
 #include "labyrinthAPI.h"
 #include <unistd.h>
 #include <string.h>
+#define JOUEUR 8
+#define ADVER 7
+#define TRESOR 6
 
 typedef struct{
    int x; //Coordonné x
    int y; //Coordonnée y
    int energie;
+   char symbole; //Symbole du joueur
 } t_joueur;
 extern int debug;	/* hack to enable debug messages */
 
 
-char** init_lab(char *labData,t_joueur p1,t_joueur p2,int sizeX, int sizeY)
+char** init_lab(char *labData,t_joueur p1,t_joueur p2,t_joueur tresor,int sizeX, int sizeY)
 {
    /*initialisation du tableau 2D */
    char** tabData;
@@ -37,9 +42,9 @@ char** init_lab(char *labData,t_joueur p1,t_joueur p2,int sizeX, int sizeY)
       {
 	 if((i==p1.y && j==p1.x) || (i==p2.y && j==p2.x) || (i==sizeY/2 && j==sizeX/2))
 	 {
-	    if((i==p1.y && j==p1.x)) tabData[i][j]='>';
-	    else if((i==p2.y && j==p2.x)) tabData[i][j]='@';
-	    else if((i==sizeY/2 && j==sizeX/2)) tabData[i][j]='T';
+	    if((i==p1.y && j==p1.x)) tabData[i][j]=JOUEUR;
+	    else if((i==p2.y && j==p2.x)) tabData[i][j]=ADVER;
+	    else if((i==tresor.y && j==tresor.x)) tabData[i][j]=TRESOR;
 	    // putchar(tabData[i][j]);
 	    //printf("|");
 	 }
@@ -54,19 +59,17 @@ char** init_lab(char *labData,t_joueur p1,t_joueur p2,int sizeX, int sizeY)
    return tabData;
 }
 
-void affichage_2D(char** labData_2D,t_joueur p1,t_joueur p2,int sizeX,int sizeY)
+void affichage_2D(char** labData_2D,t_joueur p1,t_joueur p2,t_joueur tresor,int sizeX,int sizeY)
 {
    int i,j;   
    for(i=0;i<sizeY;i++)
    {
       for(j=0;j<sizeX;j++)
       {
-	 if((i==p1.y && j==p1.x) || (i==p2.y && j==p2.x) || (i==sizeY/2 && j==sizeX/2)) 
-	 {
-	    putchar(labData_2D[i][j]);
-	    printf("|");
-	 }
-	    else 
+	 if(i==p1.y && j==p1.x) printf(">|");
+	 else if(i==p2.y && j==p2.x) printf("@|");
+	 else if(i==tresor.y && j==tresor.x) printf("T|");
+	 else 
 	    {
 	       //putchar(labData_2D[i][j]);
 	       //printf("|");
@@ -78,42 +81,139 @@ void affichage_2D(char** labData_2D,t_joueur p1,t_joueur p2,int sizeX,int sizeY)
    }
 }
 
-char *maj_lab(char *labData,t_move move,int sizeX,int sizeY)
+void maj_lab(char **labData_2D,int mv,int val,t_joueur* p1,t_joueur* p2,t_joueur* tresor,int sizeX,int sizeY)
 {
-   int val=move.value,pos,i;
+   int i;
    char temp;
-   int mv=move.type;
+   
    switch(mv)
    {
       case 0: //Bouger une ligne vers la gauche
-	 pos=sizeX*(val-1);
-	 temp=labData[pos];
-	 for(i=pos;i<pos+sizeX-1;i++) labData[i]=labData[i+1];
-	 labData[i]=temp;
-	 break;
 
+	 /* Cas où JOUEUR et/ou l'ADVER et/ou TRESOR sont sur la ligne */
+	 if (p1->y == val){	/* Si on est sur la ligne */
+	    if (p2->y == val){	/* Si l'adversaire est aussi sur la ligne */
+	       labData_2D[p2->x][p2->y] = 0; // Pas sur que ça sert
+	       p2->x--;}
+	    if (tresor->y == val){ /* Si le trésor est aussi sur la ligne */
+	       labData_2D[tresor->x][tresor->y] = 0;
+	       tresor->x--;
+	    }
+	    labData_2D[p1->x][p1->y] = 0; // Pas sur que ça sert
+	    temp = labData_2D[val][0];
+	    for(i=0;i<sizeX-1;i++) labData_2D[val][i]=labData_2D[val][i+1];
+	    labData_2D[val][sizeX-1]=temp;
+	    p1->x--;
+	    labData_2D[p1->y][p1->x]=JOUEUR;
+	    labData_2D[p2->y][p2->x]=ADVER;
+	    labData_2D[tresor->y][tresor->x]=TRESOR;
+	    break;
+	 }
+
+	 /* Cas où l'adversaire et/ou TRESOR sont sur la ligne */
+	 if (p2->y == val){	/* Si l'adversaire est aussi sur la ligne */
+	    labData_2D[p2->x][p2->y] = 0; // Pas sur que ça sert
+	    p2->x--;
+	    if (tresor->y == val){ /* Si le trésor est aussi sur la ligne */
+	       labData_2D[tresor->x][tresor->y] = 0; // Pas sur que ça sert
+	       tresor->x--;
+	    }
+	    temp = labData_2D[val][0];
+	    for(i=0;i<sizeX-1;i++) labData_2D[val][i]=labData_2D[val][i+1];
+	    labData_2D[val][sizeX-1]=temp;
+	    p1->x--;
+	    labData_2D[p2->y][p2->x]=ADVER;
+	    labData_2D[tresor->y][tresor->x]=TRESOR;
+	    break;
+	 }
+
+	 /* Cas où seul le trésor est sur la ligne */
+	 if (tresor->y == val){ /* Si le trésor est aussi sur la ligne */
+	    labData_2D[tresor->x][tresor->y] = 0; // Pas sur que ça sert
+	    tresor->x--;
+	    temp = labData_2D[val][0];
+	    for(i=0;i<sizeX-1;i++) labData_2D[val][i]=labData_2D[val][i+1];
+	    labData_2D[val][sizeX-1]=temp;
+	    p1->x--;
+	    labData_2D[tresor->y][tresor->x]=TRESOR;
+	    break;
+	 }
+	 
+	 /* Cas ou personne est sur la ligne */
+	 else{
+	    temp = labData_2D[val][0];
+	    for(i=0;i<sizeX-1;i++) 
+	    {
+	       labData_2D[val][i]=labData_2D[val][i+1];
+	    }
+	    labData_2D[val][sizeX-1]=temp;
+	    break;
+	 }
       case 1: //Bouger une ligne vers la droite
-	 pos=sizeX*val-1;
-	 temp=labData[pos];
-	 for(i=pos;i>pos-sizeX+1;i--) labData[i]=labData[i-1];
-	 labData[i]=temp;
-	 break;
+	 /* Cas où JOUEUR et/ou ADVER et/ou TRESOR sont sur la ligne */
+	 if (p1->y == val){	/* Si on est sur la ligne */
+	    if (p2->y == val){	/* Si l'adversaire est aussi sur la ligne */
+	       p2->x++;
+	       if (tresor->y == val){ /* Si le trésor est aussi sur la ligne */
+		  tresor->x++;
+	       }
+	    }
+	    labData_2D[p1->x][p1->y] = 0;
+	    temp = labData_2D[val][sizeX-1];
+	    for(i=sizeX-1;i>0;i--) labData_2D[val][i]=labData_2D[val][i-1];
+	    labData_2D[val][0]=temp;
+	    p1->x++;
+	    labData_2D[p1->y][p1->x]=JOUEUR;
+	    labData_2D[p2->y][p2->x]=ADVER;
+	    labData_2D[tresor->y][tresor->x] = TRESOR;
+	    break;
+	 }
 
+	 /* Cas ou ADVER et/ou TRESOR sont sur la ligne */
+	 if (p2->y == val){	/* Si l'adversaire est aussi sur la ligne */
+	    p2->x++;
+	    if (tresor->y == val){ /* Si le trésor est aussi sur la ligne */
+	       tresor->x++;
+	    }
+	    temp = labData_2D[val][sizeX-1];
+	    for(i=sizeX-1;i>0;i--) labData_2D[val][i]=labData_2D[val][i-1];
+	    labData_2D[val][0]=temp;
+	    labData_2D[p2->y][p2->x]=ADVER;
+	    labData_2D[tresor->y][tresor->x] = TRESOR;
+	    break;
+	 }
+
+	 /* Cas ou TRESOR est sur la ligne */
+	 if (tresor->y == val){ /* Si le trésor est aussi sur la ligne */
+	    tresor->x++;
+	    temp = labData_2D[val][sizeX-1];
+	    for(i=sizeX-1;i>0;i--) labData_2D[val][i]=labData_2D[val][i-1];
+	    labData_2D[val][0]=temp;
+	    labData_2D[p2->y][p2->x]=ADVER;
+	    labData_2D[tresor->y][tresor->x] = TRESOR;
+	    break;
+	 }
+	 /* Cas ou personne est sur la ligne */
+	 else{
+	    temp = labData_2D[val][0];
+	    for(i=0;i<sizeX-1;i++) 
+	    {
+	       labData_2D[val][i]=labData_2D[val][i+1];
+	    }
+	    labData_2D[val][sizeX-1]=temp;
+	    break;
+	 }
       case 2: //Bouger une colonne vers le haut
-	 pos=val-1;
-	 temp=labData[pos];
-	 for(i=pos;i<pos-sizeX+sizeX*sizeY;i=i+sizeX) labData[i]=labData[i+sizeX];
-	 labData[i]=temp;
+	 temp = labData_2D[0][val];	 
+	 for(i=0;i<sizeY-1;i++) labData_2D[i][val]=labData_2D[i+1][val];
+	 labData_2D[sizeY-1][val]=temp;
 	 break;
       case 3: //Bouger une colonne vers le bas
-	 pos=sizeX*sizeY-sizeX+val-1;
-	 temp=labData[pos];
-	 for(i=pos;i>val-1;i=i-sizeX) labData[i]=labData[i-sizeX];
-	 labData[i]=temp;
+	 temp = labData_2D[sizeY-1][val];	 
+	 for(i=sizeY-1;i>0;i--) labData_2D[i][val]=labData_2D[i-1][val];
+	 labData_2D[sizeY-1][val]=temp;
 	 break;
-   }
-   return labData;
-   
+   }      
 }
 
 
@@ -127,10 +227,10 @@ int main()
    t_move move;						/* a move */
    int player;
    int sizeX,sizeY;
-   t_joueur p1,p2;
+   t_joueur p1,p2,tresor;
 
    /* connection to the server */
-   connectToServer( "pc4001.polytech.upmc.fr", 1234, "Gengo_Lance");
+   connectToServer( "pc4023.polytech.upmc.fr", 1234, "Gengo_Lance");
 	
 	
    /* wait for a game, and retrieve informations about it */
@@ -143,7 +243,7 @@ int main()
 	
    printf("\n");
    //printf("L%d,R%d,U%d,D%d\n",ROTATE_LINE_LEFT,ROTATE_LINE_RIGHT,ROTATE_COLUMN_UP,ROTATE_COLUMN_DOWN);
-   int mv; //mouvement choisi
+   int mv,val; //mouvement choisi
    
    if(player==0) 
    {
@@ -166,8 +266,16 @@ int main()
       p1.energie = 1;		/* On commence avec 1 energie */
    }
 
+   tresor.x=sizeX/2;
+   tresor.y=sizeY/2;
+   tresor.energie=0;
+
+   p1.symbole='8';
+   p2.symbole='7';
+   tresor.symbole='T';
+
    //labData[pos_tresor]='T';
-   labData_2D=init_lab(labData,p1,p2,sizeX,sizeY);
+   labData_2D=init_lab(labData,p1,p2,tresor,sizeX,sizeY);
    printf("\n");
      //affichage_manuel(labData,pos_J0,pos_J1,pos_tresor,sizeX,sizeY);
 
@@ -188,7 +296,7 @@ int main()
       /* } */
       system("clear");
       printLabyrinth();
-      affichage_2D(labData_2D,p1,p2,sizeX,sizeY);
+      affichage_2D(labData_2D,p1,p2,tresor,sizeX,sizeY);
       if (player==1) 
 	  {
 	 ret = getMove( &move);
@@ -200,34 +308,34 @@ int main()
       {	 
 	 player=1;
 	 printf("Mouvement souhaité Rien=%d\nGauche=%d\nDroite=%d\nHaut=%d\nBas=%d\nChoix: ",DO_NOTHING,MOVE_LEFT,MOVE_RIGHT,MOVE_UP,MOVE_DOWN);
-	 //scanf("%d",&mv);
+	 scanf("%1d %1d",&mv,&val);
       refaire:
-	 mv=rand()%8;
-	 printf("mv=%d\n",mv);
+	 //mv=rand()%8;
+	 printf("mv=%d m_val=%d\n",mv,val);
 	 switch(mv)
 	 {
-	    case 0:
-	       goto refaire;
+	    case 0: //Bouger ligne à gauche
+	       maj_lab(labData_2D,mv,val,&p1,&p2,&tresor,sizeX,sizeY);
 	       break;
-	    case 1:
-	       goto refaire;
+	    case 1: //Bouger ligne à droite
+	       maj_lab(labData_2D,mv,val,&p1,&p2,&tresor,sizeX,sizeY);
 	       break;
-	    case 2:
-	       goto refaire;
+	    case 2: //Bouger colone en haut
+	       maj_lab(labData_2D,mv,val,&p1,&p2,&tresor,sizeX,sizeY);
 	       break;
-	    case 3:
-	       goto refaire;
+	    case 3: //Bouger colone en bas
+	       maj_lab(labData_2D,mv,val,&p1,&p2,&tresor,sizeX,sizeY);
 	       break;
 
 	    /* Déplacement en haut */
 	    case 4:	       
 	       if(p1.y==0) //Si le joueur est tout en haut et veut aller en haut
 	       {
-		  if((int)labData_2D[sizeY-1][p1.x]!=1 && (int)labData_2D[sizeY-1][p1.x]!=64) 
+		  if((int)labData_2D[sizeY-1][p1.x]!=1 && (int)labData_2D[sizeY-1][p1.x]!=64) /* 64 en ASCII est @ */
 		  {
-		     labData_2D[p1.y][p1.x]=0;
-		     p1.y=sizeY-1;
-		     labData_2D[p1.y][p1.x]='>';
+		     labData_2D[p1.y][p1.x]=0; /* On met à 0 notre position */
+		     p1.y=sizeY-1;	       /* On se déplace */
+		     labData_2D[p1.y][p1.x]=JOUEUR; /* On met notre nouveau position */
 		  }		     
 		  else goto refaire;
 	       }
@@ -238,7 +346,7 @@ int main()
 		  {
 		     labData_2D[p1.y][p1.x]=0;
 		     p1.y=p1.y-1;
-		     labData_2D[p1.y][p1.x]='>';
+		     labData_2D[p1.y][p1.x]=JOUEUR;
 		  }	
 		  else goto refaire;
 	       }	       
@@ -253,7 +361,7 @@ int main()
 		  {
 		     labData_2D[p1.y][p1.x]=0;
 		     p1.y=0;
-		     labData_2D[p1.y][p1.x]='>';
+		     labData_2D[p1.y][p1.x]=JOUEUR;
 		  }	
 		  else goto refaire;
 	       } 		       
@@ -263,7 +371,7 @@ int main()
 		  {
 		     labData_2D[p1.y][p1.x]=0;
 		     p1.y=p1.y+1;
-		     labData_2D[p1.y][p1.x]='>';
+		     labData_2D[p1.y][p1.x]=JOUEUR;
 		  }	
 		  else goto refaire;
 	       }
@@ -277,7 +385,7 @@ int main()
 		  {
 		     labData_2D[p1.y][p1.x]=0;
 		     p1.x=sizeX-1;
-		     labData_2D[p1.y][p1.x]='>';
+		     labData_2D[p1.y][p1.x]=JOUEUR;
 		  }	
 		  else goto refaire;
 	       }
@@ -287,7 +395,7 @@ int main()
 		  {
 		     labData_2D[p1.y][p1.x]=0;
 		     p1.x=p1.x-1;
-		     labData_2D[p1.y][p1.x]='>';
+		     labData_2D[p1.y][p1.x]=JOUEUR;
 		  }	
 		  else goto refaire;
 	       }			  
@@ -301,7 +409,7 @@ int main()
 		  {
 		     labData_2D[p1.y][p1.x]=0;
 		     p1.x=0;
-		     labData_2D[p1.y][p1.x]='>';
+		     labData_2D[p1.y][p1.x]=JOUEUR;
 		  }	
 		  else goto refaire;
 	       }
@@ -311,7 +419,7 @@ int main()
 		  {
 		     labData_2D[p1.y][p1.x]=0;
 		     p1.x=p1.x+1;
-		     labData_2D[p1.y][p1.x]='>';
+		     labData_2D[p1.y][p1.x]=JOUEUR;
 		  }	
 		  else goto refaire;
 	       }			  
@@ -319,7 +427,7 @@ int main()
 	 }
 
 	 move.type=mv;
-	 move.value=0;
+	 move.value=val;
 	 ret = sendMove(move); //printf("pos_J0=%d\n",pos_J0);
       }
       //printLabyrinth();	
